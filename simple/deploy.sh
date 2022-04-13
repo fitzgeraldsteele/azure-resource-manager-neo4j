8cb8b3a2b7849bcf2df669348f66a1f723c1fd7e
anthonykrinsky <anthony@drumcircleanalytics.com>
2022-04-13 15:24:40 -0700

    Updated parameters to set empty to None

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -3,4 +3,4 @@
 RESOURCE_GROUP=$1
 
 az group create --name $RESOURCE_GROUP --location westus
-az deployment group create --template-file mainTemplate.json --resource-group $RESOURCE_GROUP --parameters @mainTemplateParametersCustom.json
+az deployment group create --template-file mainTemplate.json --resource-group $RESOURCE_GROUP --parameters @mainTemplateParameters.json
